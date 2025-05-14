bfc24db3a2bd3a0ab97eb0fbfe94ff03efec078d
林博仁(Buo-ren Lin) <buo.ren.lin@gmail.com>
2025-05-15 03:22:07 +0800

    feat: Support generate ready-for-use secrets
    Signed-off-by: 林博仁(Buo-ren Lin) <buo.ren.lin@gmail.com>

--- generate_secrets.sh
+++ generate_secrets.sh
@@ -11,9 +11,37 @@ print_random () {
     LC_ALL=C tr -dc 'A-Za-z0-9!#%&()*+,-./:;<=>?@[\]^_{|}~' </dev/urandom | head -c 32
 }
 
-/bin/echo -n "LLDAP_JWT_SECRET='"
-print_random
-echo "'"
-/bin/echo -n "LLDAP_KEY_SEED='"
-print_random
-echo "'"
+operation_timestamp="$(printf '%(%Y%m%d-%H%M%S)T\n')"
+
+if ! lldap_jwt_secret="$(print_random)"; then
+    printf 'Error: Unable to generate random string for LLDAP_JWT_SECRET.\n' >&2
+    exit 1
+fi
+
+if ! lldap_key_seed="$(print_random)"; then
+    printf 'Error: Unable to generate random string for LLDAP_KEY_SEED.\n' >&2
+    exit 1
+fi
+
+if test -e .env; then
+    backup_file=".env.backup-${operation_timestamp}"
+    printf \
+        'Info: Backing up existing .env file to "%s"...\n' \
+        "${backup_file}"
+    if ! cp -a .env "${backup_file}"; then
+        printf 'Error: Unable to back up existing .env file.\n' >&2
+        exit 1
+    fi
+fi
+
+sed_opts=(
+    # NOTE: SPACE is used as the separator of the s sed command
+    -e "s __LLDAP_JWT_SECRET__ ${lldap_jwt_secret} "
+    -e "s __LLDAP_KEY_SEED__ ${lldap_key_seed} "
+)
+if ! sed "${sed_opts[@]}" .env.template >.env; then
+    printf 'Error: Unable to generate .env file from template.\n' >&2
+    exit 1
+fi
+
+printf 'Operation completed successfully.\n'
